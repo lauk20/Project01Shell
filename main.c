32f940073bb9b1a7a5c07781e2440a510f6faea5
Kenny Lau <klau20@stuy.edu>
2021-11-19 11:46:47 -0500

    Worked on parse args

--- main.c
+++ main.c
@@ -3,41 +3,57 @@
 #include <string.h>
 #include <unistd.h>
 
+void print_arr(char ** aa);
+
 char ** parse_args(char * line){
 	char * copied = malloc(strlen(line));
 	strcpy(copied, line);
 	
 	char * spaceToken = strsep(&copied, " ");
 	int countArgs = 0;
-	if (spaceToken || strlen(line) > 0){
+	/*if (spaceToken || strlen(line) > 0){
 		countArgs = countArgs + 1;
-	}
+	}*/
 
 	while (spaceToken){
 		spaceToken = strsep(&copied, " ");
 		countArgs = countArgs + 1;
 	}
 	
-	printf("%d\n", countArgs);
+	//printf("%d\n", countArgs);
 	
-	char ** args = malloc(7 * 8);
+	char ** args = malloc((countArgs + 1) * 8);
 
-	char * token = 1;
-
+	char * token = strsep(&line, " "); //" ls -a"
+	
+	//printf("%s\n", token);
+	
 	int i = 0;
+	
+	if (!token){
+		args[0] = token;
+		i = 1;
+	}
+	
+	//printf("%d: %s\n", 0, args[0]);
+	
+	
 	while (token){
 		token = strsep(&line, " ");
+		//printf("%d\n", strcmp(token, " "));
 		args[i] = token;
 		//printf("%d: %s\n", i, args[i]);
 		i = i + 1;
 	}
 
 	args[i] = NULL;
+	
+
 	return args;
 }
 
 int main(){
-	char test[100] = "ls -a -l";
+	char test[100] = "     ls   ";
 
 	char ** args = parse_args(test);
 	
