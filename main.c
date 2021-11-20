a1d5f48714a8164e7db2754f0505011d576c6653
lauk20 <klau20@stuy.edu>
2021-11-19 20:20:56 -0500

    Added format_command function that formats a string to fix unedded spaces

--- main.c
+++ main.c
@@ -3,12 +3,43 @@
 #include <string.h>
 #include <unistd.h>
 
-void print_arr(char ** aa);
+/*
+cleans up char * line so that repetitive spaces are removed.
+returns a cleaned up string that should only have spaces where neccessary.
+*/
+char * format_command(char * line){
+	char * newCommand = calloc(strlen(line), 1);
+
+	int i = 0;
+	char * start = 0;
+	char * lastSpace = 0;
+	while (i < strlen(line)){
+		if (line[i] == ' '){
+				lastSpace = line + i;
+		} else {
+			if (lastSpace && start){
+				strncat(newCommand, lastSpace, 1);
+				lastSpace = 0;
+			}
+
+			if (!start){
+				start = line + i;
+				lastSpace = 0;
+			}
+
+			strncat(newCommand, line + i, 1);
+		}
+
+		i = i + 1;
+	}
+
+	return newCommand;
+}
 
 char ** parse_args(char * line){
 	char * copied = malloc(strlen(line));
 	strcpy(copied, line);
-	
+
 	char * spaceToken = strsep(&copied, " ");
 	int countArgs = 0;
 	/*if (spaceToken || strlen(line) > 0){
@@ -19,25 +50,25 @@ char ** parse_args(char * line){
 		spaceToken = strsep(&copied, " ");
 		countArgs = countArgs + 1;
 	}
-	
+
 	//printf("%d\n", countArgs);
-	
+
 	char ** args = malloc((countArgs + 1) * 8);
 
 	char * token = strsep(&line, " "); //" ls -a"
-	
+
 	//printf("%s\n", token);
-	
+
 	int i = 0;
-	
+
 	if (!token){
 		args[0] = token;
 		i = 1;
 	}
-	
+
 	//printf("%d: %s\n", 0, args[0]);
-	
-	
+
+
 	while (token){
 		token = strsep(&line, " ");
 		//printf("%d\n", strcmp(token, " "));
@@ -47,17 +78,19 @@ char ** parse_args(char * line){
 	}
 
 	args[i] = NULL;
-	
+
 
 	return args;
 }
 
 int main(){
-	char test[100] = "     ls   ";
+	char test[100] = "   ls       -a    -b";
+
+	printf("%s\n", format_command(test));
 
 	char ** args = parse_args(test);
-	
+
 	execvp(args[0], args);
-	
+
 	return 0;
 }
